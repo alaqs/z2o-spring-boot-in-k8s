23545fd0e7253e0fe34d589c19bcf4928d01b713
Ali Maqsood <alii.maqsood95@gmail.com>
2024-08-26 16:22:25 +0100

    chore: init spring boot repository

---
+++ src/main/java/com/alaqs/z2o_spring_boot_in_k8s/Z2oSpringBootInK8sApplication.java
@@ -0,0 +1,13 @@
+package com.alaqs.z2o_spring_boot_in_k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z2oSpringBootInK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z2oSpringBootInK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alaqs/z2o_spring_boot_in_k8s/Z2oSpringBootInK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaqs.z2o_spring_boot_in_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z2oSpringBootInK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
